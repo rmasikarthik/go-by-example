7e5259ef77ec3ee543f184795daabbb79bbd68a7
rmasikarthik <rmasikarthik+github@users.noreply.github.com>
2026-07-10 13:04:08 -0500

    exercise

---
+++ hello-world.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+// Basic hello world
+
+func main() {
+	fmt.Println("Hello World!")
+}
+
+//
+// To build a go project
+//
+//1. go run hello-world.go
+//
+//For Binary
+//
+//go build hello-world.go
+//
+//./hello-world
---
+++ values.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Go" + "lang")
+	fmt.Println(" 1 + 1 = ", 1+1)
+
+	// Variable initialization
+	var a = 5
+	// Variable initialization
+	b := 5
+
+	fmt.Println("a + b = ", a+b)
+
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
